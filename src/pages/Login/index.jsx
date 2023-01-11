95cf8162870b12573cea648d5662ba803fba759d
axmadullo2001TATU <axmadullo2000@gmail.com>
2023-01-11 12:45:18 +0300

    added react-router

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+export const Login = () => {
+	return (
+		<div>
+			<Navbar />
+			<h2>Login</h2>
+		</div>
+	)
+}
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Navbar from '../../components/Navbar'
+
+export const Register = () => {
+	return (
+		<div>
+			<Navbar />
+			<h2>Register</h2>
+		</div>
+	)
+}
